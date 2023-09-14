6e22d76356aff3f2eacf0472c85c932c28366d87
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-09-14 22:03:26 +0200

    Delete checkingprojectmh-heiler1991-patch-1/novPoj.js

--- checkingprojectmh-heiler1991-patch-1/novPoj.js
+++
@@ -1,8 +0,0 @@
-// class Pojistenec {
-//     constructor(jmeno, prijmeni, vek, tel){
-//         this.inputJmeno = jmeno;
-//         this.inputPrijemni = prijmeni;
-//         this.inputVek = vek;
-//         this.inputTel = tel;
-//     }
-// }
